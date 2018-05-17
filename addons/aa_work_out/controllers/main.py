c3c9b289715aa5eb9b4f69e14afe6008d1ee910d
liaoxiaoman <liaoxiaoman@126.com>
2018-05-17 16:37:48 +0800

    运动记录1.0

--- addons/aa_work_out/controllers/main.py
+++ addons/aa_work_out/controllers/main.py
@@ -9,7 +9,12 @@ class WorkOut(http.Controller):
     @http.route(['/work_out'], type='http', auth="public", website=True, methods=['POST', 'GET'])
     def work_out(self, **kw):
         if request.env.uid == request.env.ref('base.public_user').id:
-            return request.render('web.login')
+            get_param = request.env['ir.config_parameter'].sudo().get_param
+            context = {
+                'signup_enabled': get_param('auth_signup.allow_uninvited') == 'True',
+                'reset_password_enabled': get_param('auth_signup.reset_password') == 'True',
+            }
+            return request.render('web.login', context)
         values = {}
         items = request.env['work.out.item'].sudo().search([])
         data = []
@@ -23,12 +28,12 @@ class WorkOut(http.Controller):
         work_out = request.env['work.out'].sudo().search([('user_id', '=', request.env.uid), ('date', '=', fields.Datetime.now()[0:10])], limit=1)
         if kw.get('item_id') and kw.get('weight') and kw.get('times'):
             if not work_out:
-                work_out = request.env['work.out'].create({'user_id': request.env.uid})
+                work_out = request.env['work.out'].create({})
             line_data = {
                 'work_out': work_out.id,
                 'part_id': request.env['work.out.item'].search([('id', '=', int(kw.get('item_id')))]).work_out_part.id,
                 'item_id': int(kw.get('item_id')),
-                'weight': float(kw.get('weight')),
+                'weight': kw.get('weight'),
                 'times': int(kw.get('times')),
             }
             request.env['work.out.line'].create(line_data)
--- addons/aa_work_out/models/work_out.py
+++ addons/aa_work_out/models/work_out.py
@@ -6,8 +6,9 @@ from odoo import api, fields, models
 
 class WorkOut(models.Model):
     _name = 'work.out'
+    _rec_name = 'date'
+    _order = 'id desc'
 
-    name = fields.Char(u'名称')
     date = fields.Date(u'日期', default=lambda self: fields.Datetime.now())
     line_ids = fields.One2many('work.out.line', 'work_out', string=u'明细')
     user_id = fields.Many2one('res.users', string=u'用户', required=True)
@@ -19,7 +20,8 @@ class WorkOutLine(models.Model):
     work_out = fields.Many2one('work.out', string=u'记录')
     part_id = fields.Many2one('work.out.part', string=u'部位')
     item_id = fields.Many2one('work.out.item', string=u'动作')
-    weight = fields.Float(u'重量')
+    weight = fields.Selection([('0.3', u'很轻松的热身组'), ('0.8', u'做完12到16个很累'),
+                               ('1', u'八个左右就已经力竭了'), ('1.5', u'拼了老命才做了几个')], u'重量', default='1')
     times = fields.Integer(u'次数')
     total = fields.Float(u'总量', compute="_compute_total")
 
@@ -33,7 +35,7 @@ class WorkOutLine(models.Model):
     @api.depends('weight', 'times')
     def _compute_total(self):
         for line in self:
-            line.total = line.weight * line.times
+            line.total = float(line.weight) * line.times * line.item_id.score
 
 class WorkOutPart(models.Model):
     _name = 'work.out.part'
@@ -46,4 +48,5 @@ class WorkOutItem(models.Model):
     _name = 'work.out.item'
 
     name = fields.Char(u'动作名称')
+    score = fields.Integer(u'评分')
     work_out_part = fields.Many2one('work.out.part', string=u'部位')
