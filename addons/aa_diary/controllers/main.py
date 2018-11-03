03bbe3001a148c797b35e72f086bfa164b9e651a
liaoxiaoman <liaoxiaoman@126.com>
2018-11-03 15:11:44 +0800

    增加评论和颜色和分段

--- addons/aa_diary/controllers/main.py
+++ addons/aa_diary/controllers/main.py
@@ -33,6 +33,7 @@ class Diary(http.Controller):
                     'id': i.id,
                     'text': i.text[0:12]+'...',
                     'user': i.user_id.name,
+                    'user_id': i.user_id.id,
                 })
             values['diarys'] = data
             return request.render("aa_diary.index", values)
@@ -41,5 +42,40 @@ class Diary(http.Controller):
 
     @http.route(['/get_details/<int:detail_id>'], type='http', auth="public", website=True, methods=['POST', 'GET'])
     def get_details(self, detail_id, **kw):
-        detail = request.env['diary'].sudo().browse(detail_id).text
-        return request.render("aa_diary.detail", {'detail': detail})
+        # 验证登录
+        if request.env.uid == request.env.ref('base.public_user').id:
+            get_param = request.env['ir.config_parameter'].sudo().get_param
+            context = {
+                'signup_enabled': get_param('auth_signup.allow_uninvited') == 'True',
+                'reset_password_enabled': get_param('auth_signup.reset_password') == 'True',
+            }
+            return request.render('web.login', context)
+        if request.env.uid in [8, 6]:
+            diary = request.env['diary'].sudo().browse(detail_id)
+            return request.render("aa_diary.detail", {'diary': diary, 'comments': diary.comment_ids})
+        else:
+            return request.redirect('/contactus')
+
+    @http.route(['/comment'], type='http', auth="public", website=True, methods=['POST', 'GET'])
+    def comment(self, **kw):
+        # 验证登录
+        if request.env.uid == request.env.ref('base.public_user').id:
+            get_param = request.env['ir.config_parameter'].sudo().get_param
+            context = {
+                'signup_enabled': get_param('auth_signup.allow_uninvited') == 'True',
+                'reset_password_enabled': get_param('auth_signup.reset_password') == 'True',
+            }
+            return request.render('web.login', context)
+        if request.env.uid in [8, 6]:
+            if kw.get('text') and kw.get('diary_id'):
+                if not request.env['diary.comment'].sudo().search([('diary_id', '=', eval(kw.get('diary_id'))), ('text', '=', kw.get('text'))]):
+                    diary_data = {
+                        'text': kw.get('text'),
+                        'user_id': request.env.uid,
+                        'diary_id': eval(kw.get('diary_id')),
+                    }
+                    request.env['diary.comment'].create(diary_data)
+            diary = request.env['diary'].sudo().browse(eval(kw.get('diary_id')))
+            return request.render("aa_diary.detail", {'diary': diary, 'comments': diary.comment_ids})
+        else:
+            return request.redirect('/contactus')
--- addons/aa_diary/models/diary.py
+++ addons/aa_diary/models/diary.py
@@ -12,3 +12,14 @@ class Diary(models.Model):
     date = fields.Date(u'日期', default=lambda self: fields.Datetime.now())
     text = fields.Text(u'内容')
     user_id = fields.Many2one('res.users', string=u'用户', required=True)
+    comment_ids = fields.One2many('diary.comment', 'diary_id')
+
+class DiaryComment(models.Model):
+    _name = 'diary.comment'
+    _rec_name = 'date'
+    _order = 'id'
+
+    date = fields.Date(u'日期', default=lambda self: fields.Datetime.now())
+    text = fields.Text(u'内容')
+    user_id = fields.Many2one('res.users', string=u'用户', required=True)
+    diary_id = fields.Many2one('diary')
